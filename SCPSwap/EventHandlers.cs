746a65680c33189abe6c92a074087adab9e4bcbb
Franco <36207738+SrLicht@users.noreply.github.com>
2020-11-04 17:02:18 -0300

    SCP SL Shit Update

--- SCPSwap/EventHandlers.cs
+++ SCPSwap/EventHandlers.cs
@@ -7,8 +7,10 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace SCPSwap {
-	public sealed class EventHandlers {
+namespace SCPSwap
+{
+	public sealed class EventHandlers
+	{
 		private Dictionary<Player, Player> ongoingReqs = new Dictionary<Player, Player>();
 
 		private List<CoroutineHandle> coroutines = new List<CoroutineHandle>();
@@ -17,7 +19,8 @@ namespace SCPSwap {
 		private bool allowSwaps = false;
 		private bool isRoundStarted = false;
 
-		public void Start() {
+		public void Start()
+		{
 			Exiled.Events.Handlers.Player.ChangingRole += SetRole;
 			Exiled.Events.Handlers.Server.WaitingForPlayers += OnWaitingForPlayers;
 			Exiled.Events.Handlers.Server.RoundStarted += OnRoundStart;
@@ -26,7 +29,8 @@ namespace SCPSwap {
 			Exiled.Events.Handlers.Server.SendingConsoleCommand += OnConsoleCommand;
 		}
 
-		public void Stop() {
+		public void Stop()
+		{
 			Exiled.Events.Handlers.Player.ChangingRole -= SetRole;
 			Exiled.Events.Handlers.Server.WaitingForPlayers -= OnWaitingForPlayers;
 			Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStart;
@@ -78,13 +82,16 @@ namespace SCPSwap {
 
 		public EventHandlers(ScpSwap plugin) => this.plugin = plugin;
 
-		public void SetRole(ChangingRoleEventArgs ev) {
-			if(ev.NewRole.GetTeam() == Team.SCP) {
+		public void SetRole(ChangingRoleEventArgs ev)
+		{
+			if (ev.NewRole.GetTeam() == Team.SCP)
+			{
 				ev.Player.Broadcast(plugin.Config.SwapInfoHintDur, plugin.Config.SwapInfoHint);
 			}
 		}
 
-		private IEnumerator<float> SendRequest(Player source, Player dest) {
+		private IEnumerator<float> SendRequest(Player source, Player dest)
+		{
 			ongoingReqs.Add(source, dest);
 			dest.Broadcast(7, plugin.Config.SwapRequestBroadcast.Replace("%player", source.Nickname).Replace("%role2", source.Role.ToString()).Replace("%role1", dest.Role.ToString()));
 			dest.SendConsoleMessage(plugin.Config.SwapRequestConsoleMessage.Replace("%player", $"{source.ReferenceHub.nicknameSync.Network_myNickSync}").Replace("%scp", source.Role.ToString()), "yellow");//{valid.FirstOrDefault(x => x.Value == source.Role).Key}
@@ -92,8 +99,10 @@ namespace SCPSwap {
 			TimeoutRequest(source);
 		}
 
-		private void TimeoutRequest(Player source) {
-			if(ongoingReqs.ContainsKey(source)) {
+		private void TimeoutRequest(Player source)
+		{
+			if (ongoingReqs.ContainsKey(source))
+			{
 				Player dest = ongoingReqs[source];
 				source.SendConsoleMessage(plugin.Config.SwapRequestNoResponse, "red");
 				dest.SendConsoleMessage(plugin.Config.SwapRequestTimedOut, "red");
@@ -101,7 +110,8 @@ namespace SCPSwap {
 			}
 		}
 
-		private void PerformSwap(Player source, Player dest) {
+		private void PerformSwap(Player source, Player dest)
+		{
 			source.SendConsoleMessage(plugin.Config.SwapRequestSuccess, "green");
 
 			RoleType sRole = source.Role;
@@ -124,69 +134,83 @@ namespace SCPSwap {
 			ongoingReqs.Remove(source);
 		}
 
-		public void OnRoundStart() {
+		public void OnRoundStart()
+		{
 			allowSwaps = true;
 			isRoundStarted = true;
 			Timing.CallDelayed(plugin.Config.SwapTimeout, () => allowSwaps = false);
 		}
 
-		public void OnRoundRestart() {
+		public void OnRoundRestart()
+		{
 			// fail safe
 			isRoundStarted = false;
-			Timing.KillCoroutines(coroutines);
-			Timing.KillCoroutines(reqCoroutines.Values);
+			Timing.KillCoroutines(coroutines.ToArray());
+			Timing.KillCoroutines(reqCoroutines.Values.ToArray());
 			coroutines.Clear();
 			reqCoroutines.Clear();
 		}
 
-		public void OnRoundEnd(RoundEndedEventArgs ev) {
+		public void OnRoundEnd(RoundEndedEventArgs ev)
+		{
 			isRoundStarted = false;
-			Timing.KillCoroutines(coroutines);
-			Timing.KillCoroutines(reqCoroutines.Values);
+			Timing.KillCoroutines(coroutines.ToArray());
+			Timing.KillCoroutines(reqCoroutines.Values.ToArray());
 			coroutines.Clear();
 			reqCoroutines.Clear();
 		}
 
-		public void OnWaitingForPlayers() {
+		public void OnWaitingForPlayers()
+		{
 			allowSwaps = false;
 		}
 
-		public void OnConsoleCommand(SendingConsoleCommandEventArgs ev) {
+		public void OnConsoleCommand(SendingConsoleCommandEventArgs ev)
+		{
 			string name = ev.Name.ToLower();
-			if(name.Equals("scpswap") || name.Equals("intercambio") || name.Equals("intercambiar") || name.Equals("swap")) {
+			if (name.Equals("scpswap") || name.Equals("intercambio") || name.Equals("intercambiar") || name.Equals("swap"))
+			{
 				ev.Allow = false;
-				if(!isRoundStarted) {
+				if (!isRoundStarted)
+				{
 					ev.ReturnMessage = plugin.Config.SwapRequestLobby;
 					ev.Color = "red";
 					return;
-				} else if(InEvent()) {
+				}
+				else if (InEvent())
+				{
 					ev.ReturnMessage = "Estamos en evento, Licht la tiene corta. - Atte: Andrés";
 					ev.Color = "red";
 					return;
 				}
 
-				if(!(ev.Player.Team == Team.SCP)) {
+				if (!(ev.Player.Team == Team.SCP))
+				{
 					ev.ReturnMessage = plugin.Config.SwapRequestDumb;
 					ev.Color = "red";
 					return;
 				}
 
-				if(!allowSwaps) {
+				if (!allowSwaps)
+				{
 					ev.ReturnMessage = plugin.Config.SwapRequestExpired;
 					ev.Color = "red";
 					return;
 				}
 
-				switch(ev.Arguments.Count) {
+				switch (ev.Arguments.Count)
+				{
 					case 1:
-						switch(ev.Arguments[0].ToLower()) {
+						switch (ev.Arguments[0].ToLower())
+						{
 							case "yes":
 							case "si":
 							case "sí":
 							case "s":
 							case "y":
 								Player swap = ongoingReqs.FirstOrDefault(x => x.Value == ev.Player).Key;
-								if(swap != null) {
+								if (swap != null)
+								{
 									PerformSwap(swap, ev.Player);
 									ev.ReturnMessage = plugin.Config.SwapRequestSuccess;
 									Timing.KillCoroutines(reqCoroutines[swap]);
@@ -199,7 +223,8 @@ namespace SCPSwap {
 							case "no":
 							case "n":
 								swap = ongoingReqs.FirstOrDefault(x => x.Value == ev.Player).Key;
-								if(swap != null) {
+								if (swap != null)
+								{
 									ev.ReturnMessage = plugin.Config.SwapRequestDenied;
 									swap.SendConsoleMessage(plugin.Config.SwapRequestDenied, "red");
 									Timing.KillCoroutines(reqCoroutines[swap]);
@@ -213,7 +238,8 @@ namespace SCPSwap {
 							case "cancelar":
 							case "cancelo":
 							case "c":
-								if(ongoingReqs.ContainsKey(ev.Player)) {
+								if (ongoingReqs.ContainsKey(ev.Player))
+								{
 									Player dest = ongoingReqs[ev.Player];
 									dest.SendConsoleMessage(plugin.Config.SwapRequestCancelled, "red");
 									Timing.KillCoroutines(reqCoroutines[ev.Player]);
@@ -225,39 +251,45 @@ namespace SCPSwap {
 								ev.ReturnMessage = plugin.Config.SwapRequestNoneOutgoing;
 								break;
 							default:
-								if(!valid.ContainsKey(ev.Arguments[0])) {
+								if (!valid.ContainsKey(ev.Arguments[0]))
+								{
 									ev.ReturnMessage = plugin.Config.SwapRequestInvalid;
 									ev.Color = "red";
 									return;
 								}
 
-								if(ongoingReqs.ContainsKey(ev.Player)) {
+								if (ongoingReqs.ContainsKey(ev.Player))
+								{
 									ev.ReturnMessage = plugin.Config.SwapRequestPending;
 									ev.Color = "red";
 									return;
 								}
 
 								RoleType role = valid[ev.Arguments[0]];
-								if(plugin.Config.SwapBlacklist.Contains((int) role)) {
+								if (plugin.Config.SwapBlacklist.Contains((int)role))
+								{
 									ev.ReturnMessage = plugin.Config.SwapRequestBlacklisted;
 									ev.Color = "red";
 									return;
 								}
 
-								if(ev.Player.Role == role) {
+								if (ev.Player.Role == role)
+								{
 									ev.ReturnMessage = plugin.Config.SwapRequestOwnRole;
 									ev.Color = "red";
 									return;
 								}
 
 								swap = Player.List.FirstOrDefault(x => role == RoleType.Scp93953 ? x.Role == role || x.Role == RoleType.Scp93989 : x.Role == role);
-								if(swap != null) {
+								if (swap != null)
+								{
 									reqCoroutines.Add(ev.Player, Timing.RunCoroutine(SendRequest(ev.Player, swap)));
 									ev.ReturnMessage = plugin.Config.SwapRequestSent;
 									ev.Color = "green";
 									return;
 								}
-								if(plugin.Config.SwapAllowNewScps) {
+								if (plugin.Config.SwapAllowNewScps)
+								{
 									ev.Player.ReferenceHub.characterClassManager.SetPlayersClass(role, ev.Player.ReferenceHub.gameObject);
 									ev.ReturnMessage = plugin.Config.SwapRequestSuccessFree;
 									ev.Color = "green";
@@ -276,10 +308,14 @@ namespace SCPSwap {
 			}
 		}
 
-		public bool InEvent() {
-			try {
+		public bool InEvent()
+		{
+			try
+			{
 				return CerberusTweaks.Plugin.InEvent;
-			} catch(Exception) {
+			}
+			catch (Exception)
+			{
 				Log.Error("No se detectó CerberusTweaks");
 				return false;
 			}
